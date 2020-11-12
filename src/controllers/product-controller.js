ff860ed59d7b0855e800f2adf0e5585cb645dd27
Gean-Santos <geansantos_15@hotmail.com>
2020-11-12 20:50:28 -0300

    Adicionando função de remover produtos por id

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -83,7 +83,7 @@ exports.put = (req, res, next) => {
             }
         }).then(x => {
             res.status(200).send({
-                message: 'Alterado com sucesso!'
+                message: 'Produto alterado com sucesso!'
             });
         }).catch(e => {
             res.status(400).send({
@@ -94,5 +94,16 @@ exports.put = (req, res, next) => {
 }
 
 exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
+    Product
+        .findByIdAndRemove(req.body.id)
+        .then(x => {
+            res.status(200).send({
+                message: 'Produto removido com sucesso!'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao remover o produto',
+                data: e
+            });
+    });
 }
