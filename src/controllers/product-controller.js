9cf0095cdcf70531560d74fd9302a747a776c543
Gean-Santos <geansantos_15@hotmail.com>
2020-11-12 13:52:22 -0300

    Adicionando função de listar produtos ativos

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,6 +3,20 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = (req, res, next) => {
+    Product
+    .find({
+         active: true 
+        }, 'title price slug')
+    .then(data => {
+        //recuperar parametros do corpo da requisição
+        res.status(200).send(data); 
+    })
+    .catch(e => {
+        res.status(400).send(e);
+    });
+};
+
 exports.post = (req, res, next) => {
     let product = new Product(req.body);
     product
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -5,10 +5,9 @@ const router = express.Router();
 
 const controller = require('../controllers/product-controller');
 
+router.get('/', controller.get);
 router.post('/', controller.post);
-
 router.put('/:id', controller.put);
-
 router.delete('/', controller.delete);
 
 module.exports = router;
