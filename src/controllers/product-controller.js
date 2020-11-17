6e61cb2f4741cad3e682e4e933d279bb477ded5a
Gean-Santos <geansantos_15@hotmail.com>
2020-11-17 11:33:01 -0300

    Adicionando Async & Await

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,41 +1,44 @@
-'use strict'
+'use strict';
 
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/product-repository');
 
-exports.get = (req, res, next) => {
-    repository
-        .get()
-        .then(data => {
-            res.status(200).send(data); 
-        })
-        .catch(e => {
-            res.status(400).send(e);
+exports.get = async(req, res, next) => {
+    try {
+        var data = await repository.get();
+        res.status(200).send(data);   
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
         });
+    }      
 }
 
-exports.getBySlug = (req, res, next) => {
-    repository
-        .getBySlug(req.params.slug)
-        .then(data => {
-            res.status(200).send(data); 
-        })
-        .catch(e => {
-            res.status(400).send(e);
+exports.getBySlug = async(req, res, next) => {
+    try {
+        var data = await repository
+            .getBySlug(req.params.slug);
+        res.status(200).send(data); 
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
         });
+    }   
 }
 
-exports.getById = (req, res, next) => {
-    repository
-        .getById(req.params.id)
-        .then(data => {
-            res.status(200).send(data); 
-        })
-        .catch(e => {
-            res.status(400).send(e);
+exports.getById = async(req, res, next) => {
+    try{
+        var data = await repository
+            .getById(req.params.id);
+        res.status(200).send(data); 
+
+    } catch(e){
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
         });
+    }
 }
 
 exports.getByTag = (req, res, next) => {
